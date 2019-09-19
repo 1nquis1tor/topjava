6ff476a7cc50c2b28f6f3d367d60962c26133a37
1nquis1tor <konstantin.gomeniuk@gmail.com>
2019-09-19 09:43:17 +0200

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.util;
 
+//import org.graalvm.compiler.lir.LIRInstruction;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
@@ -18,7 +19,9 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+
+        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 3000);
+
 //        .toLocalDate();
 //        .toLocalTime();
     }
